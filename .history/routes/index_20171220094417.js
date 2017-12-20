de9b5bf271f1268e1b5834105868e270bdde1dea
Ophis-Thi <thi@namtech.com.au>
2017-12-20 11:10:22 +0700

    merge

---
+++ .history/routes/index_20171220094417.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'index' });
+});
+router.get('/gallery', function(req, res, next) {
+  res.render('gallery', { title: 'gallery' });
+});
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'about' });
+});
+router.get('/google-apis', function(req, res, next) {
+
+  res.render('about', { title: 'about' });
+});
+module.exports = router;
---
+++ .history/routes/index_20171220094418.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'index' });
+});
+router.get('/gallery', function(req, res, next) {
+  res.render('gallery', { title: 'gallery' });
+});
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'about' });
+});
+router.get('/google-apis', function(req, res, next) {
+  var google = require('googleapis');
+  var plus = google.plus('v1');
+  
+  var API_KEY = 'ABC123'; // specify your API key here
+  
+  plus.people.get({
+    auth: API_KEY,
+    userId: '+google'
+  }, function (err, user) {
+    console.log('Result: ' + (err ? err.message : user.displayName));
+  });
+  res.render('about', { title: 'about' });
+});
+module.exports = router;
