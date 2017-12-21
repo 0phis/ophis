66efd2f5dae76b6364b9f8c4bebbbcf44a3b3594
Ophis-Thi <thi@namtech.com.au>
2017-12-21 09:47:27 +0700

    123

--- routes/index.js
+++ routes/index.js
@@ -14,29 +14,27 @@ router.get('/about', function(req, res, next) {
   res.render('about', { title: 'about' });
 });
 router.get('/google-apis', function(req, res, next) {
-  // res.render('anaapis', { title: 'apis' });
-  fs.readFile('./myappapis-9299a542bcef.json', 'utf8', function (err,data) {
-    if (err) {
-      return console.log('err',err);
-    }
-    var key = JSON.parse(data);
-    console.log('>>>>>>>>>>>>>>>>>>',data);
-    var jwtClient = new google.auth.JWT(
-      key.client_email,
-      null,
-      key.private_key,
-      ['https://www.googleapis.com/auth/analytics'], // an array of auth scopes
-      null
-    );
-    
-   jwtClient.authorize(function (err, tokens) {
-    console.log('??????????????????',tokens);
-        console.log(err,tokens);
-      if (err) {
-        console.log(err);
-        return;
-      }
-    });
-  });
+  
+  var analytics = google.analytics('v3');
+  var OAuth2Client = google.auth.OAuth2;
+  
+  // Client ID and client secret are available at
+  // https://code.google.com/apis/console
+  var CLIENT_ID = '358569257582-8s3ev0umpdh5ivuel0a5qt1bltjq1r0n.apps.googleusercontent.com';
+  var CLIENT_SECRET = '7y2Mz5elAd2Wl8t5CvWa6tfv';
+  var REDIRECT_URL = ["http://website-demo.ddns.net/oauth2callback"];
+  var oauth2Client = new OAuth2Client(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
+  
+  oauth2Client.credentials = 
+  {
+      access_token: 'ya29.GlwpBUKi3ExRlbVi_xhPYfhPk5a1sHl3PFz-1yDazDQZtdLjGoLasfWtu65S5_W6GgUsT1EKz-cYx_JmqS1mdGT8bidFsYB_X119B5GrXwzsFQ9k7AzEM7EW5l1A_A'
+  };
+  console.log(oauth2Client);
+  // res.render('anaapis', { title: 'apis' }); 
+});
+router.get('/oauth2callback', function(req, res, next) {
+  //  res.render('anaapis', { title: 'apis' }); 
+       console.log(res);
+       console.log(req);   
 });
 module.exports = router;
