3a846752515bd1ec50dc45bb9baf9299ed94fd30
Ophis-Thi <thi@namtech.com.au>
2017-12-21 17:12:09 +0700

    test6413

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var google = require('googleapis');
 var request = require('request');
-var GA = google.analytics('v3');
+var analytics = google.analytics('v3');
 var fs = require('fs');
 var router = express.Router();
 
@@ -16,31 +16,38 @@ router.get('/about', function(req, res, next) {
   res.render('about', { title: 'about' });
 });
 router.get('/google-apis', function(req, res, next) {
-//   var analytics = google.analytics('v3');
-//   var key =JSON.parse( fs.readFileSync('myappapis-9299a542bcef.json'));
-// var VIEW_ID = 'ga:166533939';
-// var jwtClient = new google.auth.JWT(
-//   'servicemyapp@myappapis.iam.gserviceaccount.com', null, key.private_key, ['https://www.googleapis.com/auth/analytics.readonly'], null);
-// jwtClient.authorize(function (err, tokens) {
-//               if (err) {
-//                     console.log(err);
-//               } else {
-//                   analytics.data.ga.get({
-//                       'auth': jwtClient,
-//                       'ids': VIEW_ID,
-//                       'metrics': 'ga:sessions', 
-//                       'start-date': '7daysAgo', 
-//                       'end-date': 'today', 
-//                       'dimensions': 'ga:date', 
-//                       }, function (err, response) {
-//                       if (err) {
-//                           console.log(err);
-//                       } else {  
-//                           console.log(JSON.stringify(response, null, 4));
-//                       }
-//                   });
-//               }
-//           });
+  var key =JSON.parse( fs.readFileSync('myappapis-9299a542bcef.json'));
+ var VIEW_ID = 'ga:166533939';
+    let jwtClient = new google.auth.JWT(
+      key.client_email, null, key.private_key,
+      ['https://www.googleapis.com/auth/analytics.readonly'], null);
+    jwtClient.authorize(function (err, tokens) {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      let analytics = google.analytics('v3');
+      queryData(analytics);
+    });
+    function queryData(analytics) {
+      analytics.data.ga.get({
+        'auth': jwtClient,
+        'ids': VIEW_ID,
+        'metrics': 'ga:uniquePageviews',
+        'dimensions': 'ga:pagePath',
+        'start-date': '30daysAgo',
+        'end-date': 'yesterday',
+        'sort': '-ga:uniquePageviews',
+        'max-results': 10,
+        'filters': 'ga:pagePath=~/ch_[-a-z0-9]+[.]html$',
+      }, function (err, response) {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        console.log(JSON.stringify(response, null, 4));
+      });  
+    }
 // res.render('anaapis', { title: 'apis' }); 
 });
 router.get('/google', function(req, res, next) {
