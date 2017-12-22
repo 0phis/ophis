168b5434b7e365aefa98a672f98c5a42538ed5ba
Ophis-Thi <thi@namtech.com.au>
2017-12-22 10:54:13 +0700

    xong toi

--- routes/index.js
+++ routes/index.js
@@ -20,9 +20,7 @@ router.get('/google-apis', function(req, res, next) {
  var VIEW_ID = 'ga:166533939';
     let jwtClient = new google.auth.JWT(
       key.client_email, null, key.private_key,
-      ['https://www.googleapis.com/auth/analytics.readonly',
-      'https://www.googleapis.com/auth/drive',
-      'https://www.googleapis.com/auth/spreadsheets'], null);
+      ['https://www.googleapis.com/auth/analytics.readonly'], null);
     jwtClient.authorize(function (err, tokens) {
       if (err) {
         console.log(err);
@@ -34,20 +32,18 @@ router.get('/google-apis', function(req, res, next) {
       analytics.data.ga.get({
         'auth':jwtClient,
         'ids': VIEW_ID,
-        'metrics': 'ga:uniquePageviews',
-        'dimensions': 'ga:pagePath',
+        'metrics': 'ga:sessions,ga:totalEvents,ga:uniqueEvents,ga:uniquePageviews',
         'start-date': '30daysAgo',
         'end-date': 'yesterday',
-        'sort': '-ga:uniquePageviews',
         'max-results': 10,
-        'filters': 'ga:pagePath=~/ch_[-a-z0-9]+[.]html$'
       }, function (err, response) {
         if (err) {
           console.log(err);
           return;
         }
         // console.log('...................');
-       console.log(JSON.stringify(response, null, 4));
+      //  console.log(JSON.stringify(response, null, 4));
+      console.log(JSON.stringify(response, null, 4));
       });  
     }
 // res.render('anaapis', { title: 'apis' }); 
